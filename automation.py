f325af911a1e3902c03b646f8df37e88a3c0c2a2
pablodelarco <pablodelarco1@gmail.com>
2023-11-13 15:09:32 +0200

    Updated Selenium script

--- automation.py
+++ automation.py
@@ -2,27 +2,40 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-chrome_browser = webdriver.Chrome()
-chrome_browser.maximize_window()
-chrome_browser.get("https://demo.seleniumeasy.com/basic-first-form-demo.html")
 
-assert "Selenium Easy Demo" in chrome_browser.title
-show_message_button = chrome_browser.find_element(By.CLASS_NAME, "btn-primary")
+def run_selenium_demo():
+    try:
+        # Initialize Chrome browser
+        chrome_browser = webdriver.Chrome()
+        chrome_browser.maximize_window()
 
-assert "Show Message" in chrome_browser.page_source
+        # Navigate to the Selenium Easy Demo page
+        chrome_browser.get("https://demo.seleniumeasy.com/basic-first-form-demo.html")
 
-user_message = chrome_browser.find_element(By.ID, "user-message")
-# chrome_browser.find_element(By.CLASS_NAME, "btn-default")
-time.sleep(2)
-user_message.clear()
-user_message.send_keys("THIS IS THE TEXT")
-time.sleep(2)
-show_message_button.click()  # this is the button that says "Show Message"
-output_message = chrome_browser.find_element(By.ID, "display")
+        # Enter a user message and click "Show Message" button
+        user_message = chrome_browser.find_element(By.ID, "user-message")
+        time.sleep(2)
+        user_message.clear()
+        user_message.send_keys("THIS IS THE TEXT")
 
-assert "THIS IS THE TEXT" in output_message.text
+        time.sleep(2)
+
+        show_message_button = chrome_browser.find_element(By.CLASS_NAME, "btn-primary")
+        show_message_button.click()
+
+        # Verify the displayed output
+        output_message = chrome_browser.find_element(By.ID, "display")
+        assert "THIS IS THE TEXT" in output_message.text
+
+        # Close popup if present
+        chrome_browser.implicitly_wait(5)
+        popup = chrome_browser.find_element(By.ID, "at-cv-lightbox-close")
+        popup.click()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        chrome_browser.quit()
 
 
-chrome_browser.implicitly_wait(5)
-popup = chrome_browser.find_element(By.ID, "at-cv-lightbox-close")
-popup.click()
+if __name__ == "__main__":
+    run_selenium_demo()
